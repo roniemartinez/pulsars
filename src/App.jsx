a2449c4fc1dcbdf5fa1c853071f8a9cc350c603d
Ronie Martinez <ronmarti18@gmail.com>
2023-03-27 01:00:25 +0200

    🔧 Hide unsupported fortune-sheet features (#8)
    * 🔧 Hide unsupported fortune-sheet features
    
    - Update README
    - Refactor CSS
    
    * Add new line

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,63 @@ import "./App.css";
 import {Workbook} from "@fortune-sheet/react";
 import "@fortune-sheet/react/dist/index.css";
 
+const toolbarItems = [
+    "undo",
+    "redo",
+    // "format-painter",
+    // "clear-format",
+    "|",
+    // "currency-format",
+    // "percentage-format",
+    // "number-decrease",
+    // "number-increase",
+    "format",
+    "font",
+    "font-size",
+    "|",
+    "bold",
+    "italic",
+    "strike-through",
+    // "underline",
+    "|",
+    "font-color",
+    // "background",
+    // "border",
+    // "merge-cell",
+    // "|",
+    // "horizontal-align",
+    // "vertical-align",
+    // "text-wrap",
+    // "text-rotation",
+    // "|",
+    // "freeze",
+    // "sort",
+    // "image",
+    // "comment",
+    // "quick-formula",
+];
+
+const cellContextMenu = [
+    "copy",
+    "paste",
+    "|",
+    "insert-row",
+    "insert-column",
+    "delete-row",
+    "delete-column",
+    "delete-cell",
+    "hide-row",
+    "hide-column",
+    // "clear",
+    // "sort",
+    // "filter",
+    // "chart",
+    // "image",
+    // "link",
+    // "data",
+    // "cell-format"
+];
+
 const App = () => {
     const [data, setData] = useState(null);
 
@@ -35,6 +92,8 @@ const App = () => {
         }}
         lang="en"
         defaultFontSize={11}  // align with umya-spreadsheet
+        toolbarItems={toolbarItems}
+        cellContextMenu={cellContextMenu}
     /> : null;
 };
 
