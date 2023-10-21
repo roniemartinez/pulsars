e68cd92ba05c16cc181e0b9686beef2f1b6ddb6f
Ronie Martinez <ronmarti18@gmail.com>
2023-10-21 10:09:44 +0100

    🔒 Patch Tauri vulnerability and update dependencies

--- vite.config.js
+++ vite.config.js
@@ -8,16 +8,16 @@ export default defineConfig({
     // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
     // prevent vite from obscuring rust errors
     clearScreen: false,
-    // tauri expects a fixed port, fail if that port is not available
+    // Tauri expects a fixed port, fail if that port is not available
     server: {
         port: 1420,
         strictPort: true,
     },
-    // to make use of `TAURI_DEBUG` and other env variables
+    // to access the Tauri environment variables set by the CLI with information about the current target
     // https://tauri.studio/v1/api/config#buildconfig.beforedevcommand
-    envPrefix: ["VITE_", "TAURI_"],
+    envPrefix: ["VITE_", "TAURI_PLATFORM", "TAURI_ARCH", "TAURI_FAMILY", "TAURI_PLATFORM_VERSION", "TAURI_PLATFORM_TYPE", "TAURI_DEBUG"],
     build: {
-    // Tauri supports es2021
+        // Tauri supports es2021
         target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
         // don't minify for debug builds
         minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
